4b8e78462abe2cfbd697993c28c4ed6dfe289ea8
locnelor <locnelor@icloud.com>
2024-11-08 08:55:14 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import HomePage from '../pages/HomePage.vue'
+
+const routes = [
+  {
+    path: "/",
+    component: HomePage
+  }
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+export default router
