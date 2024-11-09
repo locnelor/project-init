5aa795c1c289bd266d52d9026234673364c6671b
locnelor <locnelor@icloud.com>
2024-11-09 14:12:34 +0800

    init

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,29 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from '@app/prisma';
 import { HashModule } from '@app/hash';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, HashModule],
-  providers: [AuthService],
+  imports: [
+    PrismaModule,
+    HashModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return ({
+          secret: "sbppk",
+          signOptions: {
+            expiresIn: configService.get("EXPIRES_IN")
+          }
+        })
+      },
+      inject: [ConfigService]
+    })
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule { }
